c539f5cae0258a478892115a5c65d602e7034215
KanhaiyaPandey <kanhaiyapandey2232@gmail.com>
2023-12-16 07:56:47 +0530

    fetching result for search result component.

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,6 +1,36 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-undef */
+import { useEffect } from "react";
+import { fetchData } from "../utils/helpers";
 
 
-const SearchResults = () => {
+const SearchResults = ({searchQuery}) => {
+
+    const [results, setResults] = useState([]);
+    const [minPrice, setMinPrice] = useState('');
+    const [maxPrice, setMaxPrice] = useState('');
+    const [filteredResults, setFilteredResults] = useState([]);
+
+    useEffect(() => {
+
+        const fetchCardData = async () => {
+          try {
+            const data = await fetchData(searchQuery);
+            setResults(data);
+          } catch (error) {
+            console.error("Error fetching data:", error);
+          }
+        };
+    
+        fetchCardData();
+      
+      }, [searchQuery]);
+
+
+    
+
+
+
   return (
     <div>SearchResults</div>
   )
