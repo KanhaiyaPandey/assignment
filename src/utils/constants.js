c40dcb723d0247dc5e91176ea5997dca45a09f94
KanhaiyaPandey <kanhaiyapandey2232@gmail.com>
2023-12-16 07:44:42 +0530

    setting the fetch and api key.

---
+++ src/utils/constants.js
@@ -0,0 +1,1 @@
+export const Key = 'eeb4fdcc4a79c956ffa139a7bc060a832b27670595ffdabfff08c825b7ce314d';
---
+++ src/utils/helpers.js
@@ -0,0 +1,27 @@
+import { Key } from "./constants";
+
+
+export const fetchData = async (searchQuery) => {
+    try {
+      const apiKey = Key;
+      const apiUrl = "https://serpapi.com/search";
+      const queryString = new URLSearchParams({
+        engine: "google_shopping",
+        q: searchQuery || "h&m",
+        api_key: apiKey,
+      });
+  
+      const fetchUrl = `${apiUrl}?${queryString}`;
+      const response = await fetch(fetchUrl);
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      return data?.shopping_results;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      throw error; 
+    }
+  };
